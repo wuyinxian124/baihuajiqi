5d4bd21ec24c686fa9306be4c3dbb7c94d0a069d
runzhouwu <runzhouwu@tencent.com>
2024-07-23 19:25:11 +0800

    添加语音识别简单例子，使用pipeline

---
+++ huggingface/pipeline/pipeline_002.py
@@ -0,0 +1,13 @@
+import torch
+from transformers import pipeline
+
+# 自动语音识别
+speech_recognizer = pipeline("automatic-speech-recognition", model="facebook/wav2vec2-base-960h")
+
+# 加载数据集
+from datasets import load_dataset, Audio
+
+dataset = load_dataset("PolyAI/minds14", name="en-US", split="train")
+dataset = dataset.cast_column("audio", Audio(sampling_rate=speech_recognizer.feature_extractor.sampling_rate))
+result = speech_recognizer(dataset[:4]["audio"])
+print([d["text"] for d in result])
