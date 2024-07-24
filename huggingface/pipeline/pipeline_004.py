94e8981e3fc1a62b7eea1e5a9b8673fc91a73586
runzhouwu <runzhouwu@tencent.com>
2024-07-24 11:43:49 +0800

    添加认证访问模型

--- huggingface/pipeline/pipeline_004.py
+++ huggingface/pipeline/pipeline_004.py
@@ -1,10 +1,7 @@
 # 直接使用huggingface 官网 使用指南,构建例子
-# 加载需要认证的模型
 from transformers import AutoModelForSequenceClassification,AutoTokenizer,pipeline
-model_name = 'maidalun1020/bce-reranker-base_v1'
-access_token='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'
-model = AutoModelForSequenceClassification.from_pretrained(model_name,token=access_token)
-tokenizer = AutoTokenizer.from_pretrained(model_name,token=access_token)
+model = AutoModelForSequenceClassification.from_pretrained('uer/roberta-base-finetuned-chinanews-chinese')
+tokenizer = AutoTokenizer.from_pretrained('uer/roberta-base-finetuned-chinanews-chinese')
 text_classification = pipeline('sentiment-analysis', model=model, tokenizer=tokenizer)
 results = text_classification("深圳的小学生都开始放假了，爸爸妈妈有的忙了")
 for result in results:
