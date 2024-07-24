4ffcfc8d0b87106339758fdd290119e19586d9e8
runzhouwu <runzhouwu@tencent.com>
2024-07-24 18:44:34 +0800

    token 添加构建张量

--- huggingface/pipeline/pipeline_tokenizer_004.py
+++ huggingface/pipeline/pipeline_tokenizer_004.py
@@ -12,13 +12,18 @@ batch_sentences = [
 ]
 # encoded_inputs = tokenizer(batch_sentences)
 '''
+填充
 句子的长度并不总是相同，这可能会成为一个问题，因为模型输入的张量需要具有统一的形状。填充是一种策略，通过在较短的句子中添加一个特殊的padding token，以确保张量是矩形的。
 将 padding 参数设置为 True，以使批次中较短的序列填充到与最长序列相匹配的长度
 
+截断
 另一方面，有时候一个序列可能对模型来说太长了。在这种情况下，您需要将序列截断为更短的长度。
 将 truncation 参数设置为 True，以将序列截断为模型接受的最大长度.
+
+构建张量
+tokenizer可以返回实际输入到模型的张量
 '''
-encoded_inputs = tokenizer(batch_sentences,padding=True,truncation=True)
+encoded_inputs = tokenizer(batch_sentences,padding=True,truncation=True,return_tensors="pt")
 print(encoded_inputs)
 
 '''
